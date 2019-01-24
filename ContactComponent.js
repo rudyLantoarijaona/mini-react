208c4417d249ea1d5b60c8b932b34dbb3edd3fa6
Antoine LUCAS <antoinelucas@macbook-pro-de-antoine-2.home>
2019-01-24 23:42:51 +0100

    debug formulaire

--- ContactComponent.js
+++ ContactComponent.js
@@ -2,15 +2,15 @@ import BaseComponent from "./BaseComponent.js";
 import HeaderComponent from "./HeaderComponent.js";
 import FooterComponent from "./FooterComponent.js";
 import ContentComponent from "./ContentComponent.js";
-import style from "./src/assets/style.css";
 export default function ContactComponent() {
     BaseComponent.apply(this);
 
     this.render = () => {
         const header = new HeaderComponent();
         const footer = new FooterComponent();
-        let date = new Date();
+        const content = new ContentComponent();
 
+        let date = new Date();
         let form = '<div class="subscribe_form"> <div class="sub"><h1>Newsletter</h1><h4>Inscrivez-vous à notre newsletter</h4><form action="" method="post"><p><input type="text" name="Nom" id="nom" required placeholder="Nom"></p><p><input type="email" name="email" id="email" required placeholder="Email"></p><button type="submit" name="submit" id="submit">Soumettre</button></form></div></div>';
 
         const data = {
