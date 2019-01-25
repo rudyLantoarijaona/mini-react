64b15383e8e1a5757f0a273454d2d4e6293e0009
romainbor <bordier.romain@yahoo.fr>
2019-01-25 09:10:33 +0100

    supp useless file & console.log

--- RooterComponent.js
+++ RooterComponent.js
@@ -2,7 +2,7 @@ import HomeComponent from "./HomeComponent.js";
 import ContactComponent from "./ContactComponent.js";
 const homePage = new HomeComponent();
 const contactPage = new ContactComponent();
-console.log(homePage.display())
+
 export const routes = [
     {
         path: '',
